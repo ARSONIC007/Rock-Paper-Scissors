327cd510b3b23919d2da3914a0f591cc90d7ede1
Hit Patel <hitpatel0710@gmail.com>
2026-02-18 13:57:09 +0530

    do the Playable game for 1 Round

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello, World!");
-
 let getComputerChoice;
 
 let rNum = Number(Math.random().toFixed(3));
@@ -12,4 +10,42 @@ let rNum = Number(Math.random().toFixed(3));
       ? (getComputerChoice = "Scissor")
       : console.log("Error");
 
-console.log(getComputerChoice);
+let getHumanChoice = prompt("Enter Your Choice", `Rock or Paper or Scissor`);
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    // make input case-insensitive
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    // draw
+    if (humanChoice === computerChoice) {
+      console.log("It's a Draw!");
+      return;
+    }
+
+    // human win conditions
+    if (
+      (humanChoice === "rock" && computerChoice === "scissor") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissor" && computerChoice === "paper")
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+  }
+  const humanSelection = getHumanChoice;
+  const computerSelection = getComputerChoice;
+
+  playRound(humanSelection, computerSelection);
+
+  console.log(
+    `Human score is ${humanScore} & ComputerScore is ${computerScore}`,
+  );
+}
